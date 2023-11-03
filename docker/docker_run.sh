55e84f1ce1a6434fe058e79faa402c1b1f44dd84
deboleena <m23csa008@iitj.ac.in>
2023-11-03 21:10:18 +0530

    docker

---
+++ docker/docker_run.sh
@@ -0,0 +1,6 @@
+# Build the docker file 
+sudo docker build -t digits:v1 -f ./docker/Dockerfile .
+# Create out volume
+sudo docker volume create trainmach
+# Mount our volume to models directory (where train data is stored)
+sudo docker run -v trainmach:/digits/models digits:v1
